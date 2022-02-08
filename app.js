b6fa96e11adc9f90a3bda2441eb86ce0e83b8eb2
QuentinMSI <quentin.pisli@gmail.com>
2022-02-08 15:55:30 +0100

    cursor sympa

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+$(document).ready(function() {
+
+    $( "html" ).mouseenter(function() {
+
+        if (!$('#circle').hasClass("circle")) {
+            $('#circle').addClass("circle")
+        }
+
+        var mouseX = 0, mouseY = 0;
+        var xp = 0, yp = 0;
+         
+        $(document).mousemove(function(e){
+          mouseX = e.pageX - 15;
+          mouseY = e.pageY - 15; 
+        });
+          
+        setInterval(function(){
+          xp += ((mouseX - xp)/6);
+          yp += ((mouseY - yp)/6);
+          $("#circle").css({left: xp +'px', top: yp +'px'});
+        }, 20);
+    })
+
+    $( "html" ).mouseleave(function() {
+
+        if ($('#circle').hasClass("circle")) {
+            $('#circle').removeClass("circle")
+        }
+
+    })
+
+  
+});
+
 var CallBackGetSuccess = function(data){
     console.log("Donnée API", data)
 
